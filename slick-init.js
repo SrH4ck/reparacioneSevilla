c9b2d9cce0249d5b9a9237d3215541c5cc18ab60
Antonio Mesa <antonio.iesgerena@gmail.com>
2024-06-21 18:47:54 +0200

    carousel creation

---
+++ slick-init.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
